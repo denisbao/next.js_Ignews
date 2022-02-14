970335bfc495481f91f1cd87916338084fac0b34
denisbao <denis.tpd@gmail.com>
2022-02-14 15:26:20 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início</title>
+        <title>Home</title>
       </Head>
-      <h1>
-        Hello World!
-      </h1>
+      <main className={styles.contectContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, Welcome!</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
