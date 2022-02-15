5fb7783b872c32b917645ed66bdae45ffc043182
denisbao <denis.tpd@gmail.com>
2022-02-15 09:23:04 -0300

    Integrando Stripe e buscando valor por SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetServerSideProps} from 'next'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,12 +26,35 @@ export default function Home() {
           <h1>News about the <span>React</span> world</h1>
           <p>
             Get access to all the publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   )
+}
+
+// NEXT.JS: SERVER SIDE RENDERING (SSR)
+
+// a função precisa obrigatoriamente possuir esse nome
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  // chamada para a api do Stripe (serviço de pagamento)
+  const price = await stripe.prices.retrieve('price_1KTPNTH7YPkY1ILwBv9ZgRY9')
+
+  const product = {
+    price: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
