b0c762f69cd3367123c1bdc5ff2d6171e1cf00b2
denisbao <denis.tpd@gmail.com>
2022-02-15 15:17:51 -0300

    Home com Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps} from 'next'
+import { GetStaticProps} from 'next'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -36,10 +36,10 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-// NEXT.JS: SERVER SIDE RENDERING (SSR)
+// ======================== NEXT.JS: SERVER SIDE RENDERING (SSR) ========================
 
 // a função precisa obrigatoriamente possuir esse nome
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   // chamada para a api do Stripe (serviço de pagamento)
   const price = await stripe.prices.retrieve('price_1KTPNTH7YPkY1ILwBv9ZgRY9')
@@ -55,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
