4fe77d95d92e3df173947be15108b07311e388ab
denisbao <denis.tpd@gmail.com>
2022-02-16 16:12:33 -0300

    Verificando customer já existente no Stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,20 +1,59 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as db } from 'faunadb'
 import { getSession } from 'next-auth/react'
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
 
     const session = await getSession({ req })
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      // metadata
-    })
+    // recuperando todos dados do usuario logado
+    const user = await fauna.query<User>(
+      db.Get(
+        db.Match(
+          db.Index('user_by_email'),
+          db.Casefold(session.user.email)
+        )
+      )
+    )
+
+    // recupera o id do usuário no stripe
+    let customerId = user.data.stripe_customer_id
+
+    // se o usuário não tiver cadastro no stripe, faz o cadastro e atualiza no faunadb
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      })
+
+      await fauna.query (
+        db.Update(
+          db.Ref(db.Collection('users'), user.ref.id),
+          {
+            data:{
+              stripe_customer_id: stripeCustomer.id,
+            }
+          }
+        )
+      )
+      customerId = stripeCustomer.id
+    }
 
     const stripeChechoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
