4d0109ba37eb7cbbcaf008883edec7baddf63bc8
denisbao <denis.tpd@gmail.com>
2022-02-21 09:25:11 -0300

    Mostrando conteúdo do post com SSR

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,68 @@
-export default function Post() {
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/react"
+import Head from "next/head"
+import { RichText } from "prismic-dom"
+import { getPrismicClient } from "../../services/prismic"
+
+import styles from './post.module.scss'
+
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
   return (
-    <h1>Teste</h1>
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: post.content }} />
+        </article>
+      </main>
+    </>
   )
+}
+
+
+
+// CARREGAMENTO DO CONTEÚDO DO POST:
+// Essa página não pode ser gerada estaticamente porque dessa forma não é
+// possível fazer o controle de acesso ao conteúdo apenas para usuários com
+// uma assinatura ativa
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req })
+  const { slug } = params
+
+  // Recuperação do conteúdo do post pelo Prismic CMS
+  const prismic = getPrismicClient(req)
+  const response = await prismic.getByUID<any>('publication', String(slug), {})
+
+  // Formatação dos dados
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+  
 }
