ceff3a6561ac2afbd5d1e6a150369d38876d404e
denisbao <denis.tpd@gmail.com>
2022-03-02 09:37:02 -0300

    Adicionando config do Jest para arquivos de estilização

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,8 @@ module.exports = {
   transform: {                                                            // definição dos arquivos que devem ser convertidos pelo babel-jest
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
+  moduleNameMapper: {                                                     // define a biblioteca que deve ser usada para conversão dos arquivos de estilo
+    "\\.(scss|css|sass)$": "identity-obj-proxy"
+  },
   testEnvironment: 'jsdom'                                                // ambiente a ser utilizado para rodar os testes
 }
