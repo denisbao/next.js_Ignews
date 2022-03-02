5b002669b0a461ee0a7b918963ca2e4d45c410dd
denisbao <denis.tpd@gmail.com>
2022-03-02 08:45:08 -0300

    Iniciando config de testes com Jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testIgnorePatterns: ["/node_modules/", "/.next"],                       // pastas que devem ser ignoradas
+  setupFilesAfterEnv: [                                                   // arquivos que o jest deve executar antes de rodar os testes
+    "<rootDir>/src/tests/setupTests.ts"
+  ],
+  transform: {                                                            // definição dos arquivos que devem ser convertidos pelo babel-jest
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+  testEnviroment: 'jsdom'                                                 // ambiente a ser utilizado para rodar os testes
+}
