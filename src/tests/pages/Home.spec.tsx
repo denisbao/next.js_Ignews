5eaa4fc2756780276330f62a5f2669f989bec059
denisbao <denis.tpd@gmail.com>
2022-03-02 16:15:52 -0300

    Correção no teste Home loads initial data

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -18,9 +18,9 @@ describe('Home page', () => {
 
   it('renders correctly', () => {
     render(
-      <Home product={ { priceId: 'fake-prideId', amount: 'R$99,99' } }/>
+      <Home product={ { priceId: 'fake-price-id', amount: '$10.00' } }/>
     )
-    expect(screen.getByText('for R$99,99 month')).toBeInTheDocument()
+    expect(screen.getByText('for $10.00 month')).toBeInTheDocument()
   })
 
   it('loads initial data', async () => {
@@ -29,20 +29,20 @@ describe('Home page', () => {
     retrieveStripePricesMocked.mockResolvedValueOnce({  // mockResolvedValuedOnce: porque o método 'retrieve' é originalmente assíncrono
       id: 'fake-price-id',
       unit_amount: 1000,
-    } as any) // usado para que não seja necessário passar todos os valores necessários do método retrieve
+    } as any); // usado para que não seja necessário passar todos os valores necessários do método retrieve
 
-    const response = await getStaticProps({})
+    const response = await getStaticProps({});
 
     expect(response).toEqual(
       expect.objectContaining({
         props: {
           product: {
-            priceId: 'fake-price-id',
-            amount: 'R$99,99'
+            price: 'fake-price-id',
+            amount: '$10.00'
           }
         }
       })
     )
-  })
+  });
 
 })
